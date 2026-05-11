de5d4ebd23e1ad55634909b8d3e450829201c99d
Damaen Tan <tada.maenth@gmail.com>
2026-05-11 10:20:40 +0800

    Integrate IBM WatsonxAI chatbot with Gradio
    Added a QnA chatbot using IBM WatsonxAI API integrated with Gradio.

--- gradio_demo.py
+++ gradio_demo.py
@@ -53,3 +53,66 @@ demo = gr.Interface(
 )
 
 demo.launch(server_name="127.0.0.1", server_port= 7860)
+
+# Task 4: Creating A QnA Chatbot (Using IBM WatsonxAI API Model)
+
+from ibm_watsonx_ai.foundation_models import ModelInference
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watsonx_ai import Credentials
+from langchain_ibm import WatsonxLLM
+
+model_id = 'meta-llama/llama-3-2-11b-vision-instruct' 
+project_id = "skills-network"
+
+parameters = {
+    GenParams.MAX_NEW_TOKENS : 256, 
+    GenParams.TEMPERATURE : 0.5
+}
+
+watsonx_llm = WatsonxLLM(
+    model_id = model_id,
+    url = "https://us-south.ml.cloud.ibm.com",
+    project_id = project_id,
+    params = parameters,
+)
+
+query = input("Please enter your query: ")
+print(watsonx_llm.invoke(query))
+
+# Task 4 Extended: Implementing The QnA Chatbot To Gradio (Not Through Command Prompt or Terminal)
+
+from ibm_watsonx_ai.foundation_models import ModelInference
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watsonx_ai import Credentials
+from langchain_ibm import WatsonxLLM
+import gradio as gr
+
+model_id = 'meta-llama/llama-3-2-11b-vision-instruct' 
+project_id = "skills-network"
+
+parameters = {
+    GenParams.MAX_NEW_TOKENS : 256, 
+    GenParams.TEMPERATURE : 0.5
+}
+
+watsonx_llm = WatsonxLLM(
+    model_id = model_id,
+    url = "https://us-south.ml.cloud.ibm.com",
+    project_id = project_id,
+    params = parameters,
+)
+
+def generate_response(prompt_txt):
+    generated_response = watsonx_llm.invoke(prompt_txt)
+    return generated_response
+
+chat_application = gr.Interface(
+    fn = generate_response,
+	allow_flagging = "never",
+    inputs = gr.Textbox(label = "Input", lines = 5, placeholder = "Type your question here..."),
+    outputs = gr.Textbox(label = "Output"),
+    title = "Watsonx.ai Chatbot",
+    description = "Ask any question and the chatbot will try to answer."
+)
+
+chat_application.launch(server_name = "127.0.0.1", server_port = 7860)
