497c492187f58c76748f636d05ea80b711adb4c3
Damaen Tan <tada.maenth@gmail.com>
2026-05-11 16:08:45 +0800

    Update imports in app.py for data processing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import logging
 import uuid
 import gradio as gr
 from modules.data_extraction import extract_linkedin_profile
-from modules.data_processing import split_profile_data, vector_database
+from modules.data_processing import split_profile_data, create_vector_database, verify_embeddings
 from modules.llm_interface import change_llm_model
 from modules.query_engine import generate_initial_facts, answer_user_query
 import config
