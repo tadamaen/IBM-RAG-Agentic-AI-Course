8ec68e83c15b3e43df9dee1b6b83c7daa671c890
Damaen Tan <tada.maenth@gmail.com>
2026-05-11 10:03:43 +0800

    Add sentence builder feature with multiple inputs

--- gradio_demo.py
+++ gradio_demo.py
@@ -29,3 +29,27 @@ demo = gr.Interface(
 ) 
 
 demo.launch(server_name = "127.0.0.1", server_port = 7860)
+
+# Task 3: Sentence Builder (Using Multiple Inputs With Different Gradio Interface Inputs)
+
+import gradio as gr
+
+def sentence_builder(quantity, tech_worker_type, countries, place, activity_list, morning):
+    return f"""The {quantity} {tech_worker_type}s from {" and ".join(countries)} went to the {place} where they {" and ".join(activity_list)} until the {"morning" if morning else "night"}"""
+
+demo = gr.Interface(
+    fn = sentence_builder,
+    inputs = [gr.Slider(3, 20, value = 4, step = 1, label = "Number Of Workers", info = "Choose between 3 and 20"),
+              gr.Dropdown(["Data Scientist", "Software Developer", "Software Engineer"], label = "Tech Worker Type", info = "Will add more tech worker types later!"),
+              gr.CheckboxGroup(["Canada", "Japan", "France"], label = "Countries", info = "Where are they from?"),
+              gr.Radio(["Office", "Restaurant", "Meeting Room"], label = "Location", info = "Where did they go?"),
+              gr.Dropdown(["Partied", "Brainstormed", "Coded", "Fixed Bugs"], value = ["Brainstormed", "Coded"], multiselect = True, label = "Activities", info = "Which activities did they perform?"),
+              gr.Checkbox(label = "Morning", info = "Did they carry out the activities in the morning?")],
+    outputs = "text",
+    examples = [[3, "Software Developer", ["Canada", "Japan"], "Restaurant", ["Coded", "Fixed Bugs"], True],
+                [4, "Data Scientist", ["Japan"], "Office", ["Brainstormed", "Partied"], False],
+                [10, "Software Engineer", ["Canada", "France"], "Meeting Room", ["Brainstormed"], False],
+                [8, "Data Scientist", ["France"], "Restaurant", ["Coded"], True]]
+)
+
+demo.launch(server_name="127.0.0.1", server_port= 7860)
